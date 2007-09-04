500280a804b4efa20e0e86ded8762a4c6d9f9c7c
adlr <adlr@02b73444-c424-0410-9a08-3b57f899d57e>
2007-09-04 05:50:00 +0000

    To aid in learning FormulatePro, a tips section has been added that shows a new tip each time the user starts.
    git-svn-id: http://formulatepro.googlecode.com/svn/trunk@42 02b73444-c424-0410-9a08-3b57f899d57e

---
+++ FPTipsController.h
@@ -0,0 +1,22 @@
+//
+//  FPTipsController.h
+//  FormulatePro
+//
+//  Created by Andrew de los Reyes on 9/3/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FPTipsController : NSObject {
+    NSArray *_tips;
+    unsigned int _tipOnDisplay;
+    IBOutlet NSTextField *_tipTextField;
+    IBOutlet NSWindow *_tipWindow;
+}
+
+- (IBAction)nextTip:(id)sender;
+- (IBAction)previousTip:(id)sender;
+
+@end
---
+++ FPTipsController.m
@@ -0,0 +1,108 @@
+//
+//  FPTipsController.m
+//  FormulatePro
+//
+//  Created by Andrew de los Reyes on 9/3/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "FPTipsController.h"
+#import "FPLogging.h"
+
+static NSString *kFPShowTipsAtStartup = @"FPShowTipsAtStartup";
+static NSString *kFPNextToolTip = @"FPNextToolTip";
+
+@implementation FPTipsController
+
++ (void)initialize
+{
+    NSDictionary *defaults = [NSDictionary dictionaryWithObjectsAndKeys:
+        [NSNumber numberWithBool:YES],kFPShowTipsAtStartup,
+        [NSNumber numberWithUnsignedInt:0],kFPNextToolTip,
+        nil,nil];
+    [[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:defaults];
+}
+
+- (unsigned int)getNextTipIndexFromDefaults
+{
+    id dict = [[NSUserDefaultsController sharedUserDefaultsController] values];
+    return [(NSNumber*)[dict valueForKey:kFPNextToolTip] unsignedIntValue];
+}
+
+- (void)setNextTipIndexToDefaults:(unsigned int)nextTip;
+{
+//    NSDictionary *defaults = [NSDictionary dictionaryWithObjectsAndKeys:
+//        [NSNumber numberWithUnsignedInt:nextTip],kFPNextToolTip,
+//        nil,nil];
+    id values = [[NSUserDefaultsController sharedUserDefaultsController] values];
+    [values setValue:[NSNumber numberWithUnsignedInt:nextTip] forKey:kFPNextToolTip];
+//    [[NSUserDefaultsController sharedUserDefaultsController] setInitialValues:defaults];
+//    DLog(@"about to save\n");
+//    [(NSUserDefaultsController*)[NSUserDefaultsController sharedUserDefaultsController] save:self];
+//    DLog(@"saved\n");
+}
+
+- (void)displayTip:(unsigned int)tip
+{
+    if (tip >= [_tips count])
+        tip = 0;
+    [_tipTextField setStringValue:[_tips objectAtIndex:tip]];
+    tip++;
+    if (tip >= [_tips count])
+        tip = 0;
+    [self setNextTipIndexToDefaults:tip];
+}
+
+- (void)awakeFromNib
+{
+    _tips = nil;
+    id values = [[NSUserDefaultsController sharedUserDefaultsController] values];
+    if (NO == [[values valueForKey:kFPShowTipsAtStartup] boolValue])
+        return;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"tips" ofType:@"plist"];
+    if (nil == path) {
+        NSLog(@"unable to locate tips.plist\n");
+        return;
+    }
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    if (nil == data) {
+        NSLog(@"unable to load tips.plist\n");
+        return;
+    }
+    _tips = [NSPropertyListSerialization propertyListFromData:data
+                                            mutabilityOption:NSPropertyListImmutable
+                                                      format:nil
+                                            errorDescription:nil];
+    [_tips retain];
+    _tipOnDisplay = [self getNextTipIndexFromDefaults];
+    [self displayTip:_tipOnDisplay];
+    [_tipWindow makeKeyAndOrderFront:self];
+    DLog(@"okay\n");
+}
+
+- (IBAction)nextTip:(id)sender
+{
+    _tipOnDisplay++;
+    if (_tipOnDisplay >= [_tips count])
+        _tipOnDisplay = 0;
+    [self displayTip:_tipOnDisplay];
+}
+
+- (IBAction)previousTip:(id)sender
+{
+    if (_tipOnDisplay == 0)
+        _tipOnDisplay = [_tips count];
+    _tipOnDisplay--;
+    [self displayTip:_tipOnDisplay];
+}
+
+
+- (void)windowWillClose:(NSNotification *)aNotification
+{
+    if (_tips) {
+        [_tips release];
+        _tips = nil;
+    }
+}
+
+@end
