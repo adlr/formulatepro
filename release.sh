b7a977a7292b3dafe0631192668bf5bb8b427dc9
adlr <adlr@02b73444-c424-0410-9a08-3b57f899d57e>
2008-01-21 03:12:20 +0000

    changes for release
    git-svn-id: http://formulatepro.googlecode.com/svn/tags/v0.0.4@70 02b73444-c424-0410-9a08-3b57f899d57e

--- release.sh
+++ release.sh
@@ -7,4 +7,17 @@
 # for example:
 # ./release.sh ./build/Release/FormulatePro.app FormulatePro-0.0.2 FormulatePro-0.0.2.dmg
 
-GZIP=-9 hdiutil create -fs HFS+ -srcfolder $1 -volname $2 $3
+if [ "$1" = "-v" ]; then
+	SRC="./build/Release/FormulatePro.app"
+	VOL="FormulatePro-$2"
+	OUT="$VOL.dmg"
+else
+	SRC="$1"
+	VOL="$2"
+	OUT="$3"
+fi
+
+echo "SRC = '$SRC'"
+echo "VOL = '$VOL'"
+echo "OUT = '$OUT'"
+GZIP=-9 hdiutil create -fs HFS+ -srcfolder "$SRC" -volname "$VOL" "$OUT"
