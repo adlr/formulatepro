4632b5da8424987381c97630ceb16f1078507ca6
Andrew de los Reyes <opensource@google.com>
2009-10-17 23:37:14 -0700

    Support for signing updates (if you have the private key)
    Also, add public key to build so Sparkle can use it to verify updates.

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# simple usage: $ ./release.sh -v 0.0.6
+
 # $1 - source folder
 # $2 - volume name
 # $3 - output file
@@ -11,6 +13,7 @@ if [ "$1" = "-v" ]; then
 	SRC="./build/Release/FormulatePro.app"
 	VOL="FormulatePro-$2"
 	OUT="$VOL.dmg"
+	VERS=$1
 else
 	SRC="$1"
 	VOL="$2"
@@ -20,4 +23,21 @@ fi
 echo "SRC = '$SRC'"
 echo "VOL = '$VOL'"
 echo "OUT = '$OUT'"
+echo Creating .dmg file
 GZIP=-9 hdiutil create -fs HFS+ -srcfolder "$SRC" -volname "$VOL" "$OUT"
+echo signing
+SIG=$(ruby ./third_party/Sparkle/Extras/Signing\ Tools/sign_update.rb \
+    "$OUT" ./dsa_priv.pem)
+cat <<EOF
+        <item>
+            <title>Version $VERS</title>
+            <description>http://adlr.info/appcasts/$VOL.html</description>
+            <pubDate>$(date)</pubDate>
+            <enclosure sparkle:version="$VERS"
+                url="http://adlr.info/appcasts/$VOL.dmg"
+                sparkle:dsaSignature="$SIG"
+                length="$(stat -f "%z" "$OUT")"
+                type="application/octet-stream"/>
+        </item>
+EOF
+
